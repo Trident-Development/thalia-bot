a84f20bf7b718c814f4d84ec7ef118b295094608
truongluan <truongluan303@gmail.com>
2022-10-24 00:49:01 -0700

    Code refactor

---
+++ const/command_descriptions.py
@@ -0,0 +1,6 @@
+SETUP_DAILY_JOKE = (
+    "The bot will send a joke everyday at 11:00 am PST in the current channel"
+)
+TURN_OFF_DAILY_JOKE = "Turn off the current daily joke if there is any"
+TELL_GEEK_JOKE = "Send a random programming joke"
+HELP = "Display the list of commands and their usages"
